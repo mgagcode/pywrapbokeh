6d8e9cd6cbde2121df27655d73ffd5f758da0f63
Unknown <martin.guthrie.code@gmail.com>
2018-07-20 19:02:03 -0400

    example2
    wip - before ceating new project fishTracker

--- ex2_main.py
+++ ex2_main.py
@@ -2,10 +2,25 @@
 # -*- coding: utf-8 -*-
 
 from flask import Flask
+from flask_security import Security, login_required, SQLAlchemySessionUserDatastore
+from database import db_session, init_db
+from models import User, Role
 
-
-#app = Flask(__name__, static_folder='./media')
 app = Flask(__name__, static_url_path='/media')
+app.config['SECRET_KEY'] = 'super-secret'
+
+# Setup Flask-Security
+user_datastore = SQLAlchemySessionUserDatastore(db_session, User, Role)
+security = Security(app, user_datastore)
+
+
+# Create a user to test with
+@app.before_first_request
+def create_user():
+    init_db()
+    user_datastore.create_user(email='matt@nobien.net', password='password')
+    db_session.commit()
+
 
 with app.app_context():
     from ex_utils import setup_logging
@@ -20,7 +35,9 @@ with app.app_context():
     app.register_blueprint(ex2_login)
     app.register_blueprint(ex2_login_signup)
 
-app.run(host="0.0.0.0", port=6800, debug=False)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=6800, debug=False)
 
 # consider this first graph example, https://bokeh.pydata.org/en/latest/docs/user_guide/interaction/callbacks.html
 
